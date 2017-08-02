cf861008545157e1deb94de7f649fc310c895573
Michael Wells <cybergig22@live.com>
2017-08-02 04:49:22 -0500

    re-dev
    re-dev

--- mainfiles/javascript/devcontent.js
+++ mainfiles/javascript/devcontent.js
@@ -20,68 +20,59 @@ angular.module("devcontent", [])
         proddesc: "2nd Video game I am working on",
         prodid: 2
       }
-
     };
-    $scope.currentImage = 0;
-    $scope.gamethumb = [
-      {
-        src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsNlkRGiOop_M7mpxvfiVT9S8_P0PiBRdHDYl3lQdnO8Y0zWHrSA"
-      }
-
-    ];
-
     $scope.gametitles = {
       1: {
-        //gamethumb: 'gamethumb1',
+        gamethumb: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsNlkRGiOop_M7mpxvfiVT9S8_P0PiBRdHDYl3lQdnO8Y0zWHrSA",
         gametitle: "ProjectEOB",
         gamedesc: "Video game I am working on",
         gamerelease: new Date("November 23, 1980"),
         gameid: 1
       },
       2: {
-        //gamethumb: 'gamethumb2',
+        gamethumb: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsNlkRGiOop_M7mpxvfiVT9S8_P0PiBRdHDYl3lQdnO8Y0zWHrSA",
         gametitle: "Project Gekisen",
         gamedesc: "Video game I am working on 2",
         gamerelease: new Date("To Be Announced"),
         gameid: 2
       }, //gamerelease: new Date("November 23, 1986")
       3: {
-        //gamethumb: "Thumbnail",
+        gamethumb: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsNlkRGiOop_M7mpxvfiVT9S8_P0PiBRdHDYl3lQdnO8Y0zWHrSA",
         gametitle: "Project Solar",
         gamedesc: "Video game I am working on 2",
         gamerelease: new Date("To Be Announced"),
         gameid: 3
       }, //gamerelease: new Date("November 23, 1986")
       4: {
-        //gamethumb: "Thumbnail",
+        gamethumb: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsNlkRGiOop_M7mpxvfiVT9S8_P0PiBRdHDYl3lQdnO8Y0zWHrSA",
         gametitle: "Game Title 4",
         gamedesc: "Game Description 4",
         gamerelease: new Date("To Be Announced"),
         gameid: 4
       },
       5: {
-        //gamethumb: "Thumbnail",
+        gamethumb: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsNlkRGiOop_M7mpxvfiVT9S8_P0PiBRdHDYl3lQdnO8Y0zWHrSA",
         gametitle: "Game Title 5",
         gamedesc: "Game Description 5",
         gamerelease: new Date("To Be Announced"),
         gameid: 5
       },
       6: {
-        //gamethumb: "Thumbnail",
+        gamethumb: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsNlkRGiOop_M7mpxvfiVT9S8_P0PiBRdHDYl3lQdnO8Y0zWHrSA",
         gametitle: "Game Title 6",
         gamedesc: "Game Description 6",
         gamerelease: new Date("To Be Announced"),
         gameid: 6
       },
       7: {
-        //gamethumb: "Thumbnail",
+        gamethumb: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsNlkRGiOop_M7mpxvfiVT9S8_P0PiBRdHDYl3lQdnO8Y0zWHrSA",
         gametitle: "Game Title 7",
         gamedesc: "Game Description 7",
         gamerelease: new Date("To Be Announced"),
         gameid: 7
       },
       8: {
-        //gamethumb: "Thumbnail",
+        gamethumb: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsNlkRGiOop_M7mpxvfiVT9S8_P0PiBRdHDYl3lQdnO8Y0zWHrSA",
         gametitle: "Game Title 8",
         gamedesc: "Game Description 8",
         gamerelease: new Date("To Be Announced"),
